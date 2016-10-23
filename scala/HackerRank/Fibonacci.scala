993bd5fd8e0ce781d784fb13aa058f8241d0982c
Shishir Biyyala <shishir.biyyala@gmail.com>
2016-10-23 14:51:19 -0400

    Configured ensime for scala

--- scala/HackerRank/Fibonacci.scala
+++ scala/HackerRank/Fibonacci.scala
@@ -1,13 +1,13 @@
 import scala.io.StdIn
 
 object Fibonacci {
-    
-     def fibonacci(x:Int):Int = {
-        if (x == 0 || x == 1) x
-        else fibonacci(x-1) + fibonacci(x-2)
-    }
+  
+  def fibonacci(x:Int):Int = {
+    if (x == 0 || x == 1) x
+    else fibonacci(x-1) + fibonacci(x-2)
+  }
 
-    def main(args: Array[String]) {
-         println(fibonacci(StdIn.readInt()))
-    }
+  def main(args: Array[String]) {
+    println(fibonacci(StdIn.readInt()))
+  }
 }
