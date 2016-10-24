162147973a91f02d320c9e30ee7066641972c040
Shishir Biyyala <shishir.biyyala@gmail.com>
2016-10-24 19:08:49 -0400

    [HackerRank]: Counting inversions

---
+++ java/miscellaneous/HackerRank/CountingInversions.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class CountingInversions {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            Integer arr[] = new Integer[n];
+            for(int arr_i=0; arr_i < n; arr_i++) {
+                arr[arr_i] = in.nextInt();
+            }
+
+            assert(countInversions(new Integer[] {2, 1, 3, 1, 2}) == 5);
+            assert(countInversions(new Integer[] {1, 2, 4}) == 0);
+            assert(countInversions(new Integer[] {2, 4, 1}) == 0);
+        }
+    }
+
+    private static long countInversions(Integer[] arr) {
+        Integer[] aux = new Integer[arr.length];
+        return sort(arr, aux, 0, arr.length-1);
+    }
+
+    private static long sort(Integer[] arr, Integer[] aux, int lo, int hi) {
+        
+        if (hi <= lo) return 0;
+
+        long count = 0;
+        int mid = (lo + hi)/2;
+        count += sort(arr, aux, lo, mid);
+        count += sort(arr, aux, mid+1, hi);
+        count += merge(arr, aux, lo, hi);
+        
+        return count;
+    }
+
+    private static long merge(Integer[] arr, Integer[] aux, int lo, int hi) {
+
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = arr[k]; 
+        }
+                
+        int mid = (lo + hi)/2;
+        int i = lo;
+        int j = mid+1;
+                
+        long count = 0;
+        for (int idx = lo; idx <= hi; idx++) {
+            if (i > mid) arr[idx] = aux[j++];
+            else if (j > hi) arr[idx] = aux[i++];
+            else if (aux[j] < aux[i]) {
+                count += mid + 1 - i;
+                arr[idx] = aux[j++];
+            } else arr[idx] = aux[i++];
+        }
+
+        return count;
+    }
+}
