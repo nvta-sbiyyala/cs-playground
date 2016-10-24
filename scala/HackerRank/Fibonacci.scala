6fdbe064eca924fbb07b2ecdb05598ef64ca1660
Shishir Biyyala <shishir.biyyala@gmail.com>
2016-10-23 22:51:54 -0400

    Have everything Extend App

--- scala/HackerRank/Fibonacci.scala
+++ scala/HackerRank/Fibonacci.scala
@@ -1,13 +1,29 @@
 import scala.io.StdIn
 
-object Fibonacci {
+object Fibonacci extends App {
   
-  def fibonacci(x:Int):Int = {
+  def fibonacci(x : Int) : Long = {
     if (x == 0 || x == 1) x
     else fibonacci(x-1) + fibonacci(x-2)
   }
 
-  def main(args: Array[String]) {
-    println(fibonacci(StdIn.readInt()))
+  def fibonacciIterative(x : Int) : Long = {
+    if (x == 0 || x == 1) x
+    else {
+      var x0 = 0
+      var x1 = 1
+      for (i <- 2 to x) {
+        x1 = x0 + x1
+        x0 = x1 - x0
+      }
+      x1
+    }
   }
+
+  val x: Int = StdIn.readInt()
+  println("====Recursive Fibonacci====")
+  println(fibonacci(x))
+  println("====Iterative Fibonacci====")
+  println(fibonacciIterative(x))
+
 }
