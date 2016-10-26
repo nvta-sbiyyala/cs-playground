6f5ef14f4abf4b7637b7dd6b00febbf2e2471b13
Shishir Biyyala <shishir.biyyala@gmail.com>
2016-10-26 17:52:24 -0400

    [Chore]: Naming convention

--- java/miscellaneous/HackerRank/MakingAnagrams.java
+++ java/miscellaneous/HackerRank/MakingAnagrams.java
@@ -6,29 +6,29 @@ import java.util.regex.*;
 
 public class MakingAnagrams {
     
-    public static int numberNeeded(String first, String second) {
+    static int computeLeastNumberOfRemovals(String first, String second) {
         Map<Character, Integer> firstMap = createFrequencyMap(first);
         Map<Character, Integer> secondMap = createFrequencyMap(second);
 
-        int firstNumNeeded = firstMap.entrySet()
+        int firstNum = firstMap.entrySet()
             .stream()
             .filter(x -> !secondMap.containsKey(x.getKey()))
             .mapToInt(x -> x.getValue())
             .sum();
 
-        int secondNumNeeded = secondMap.entrySet()
+        int secondNum = secondMap.entrySet()
             .stream()
             .filter(x -> !firstMap.containsKey(x.getKey()))
             .mapToInt(x -> x.getValue())
             .sum();
 
-        int commonNumNeeded = firstMap.entrySet()
+        int commonNum = firstMap.entrySet()
             .stream()
             .filter(x -> secondMap.containsKey(x.getKey()))
             .mapToInt(x -> Math.abs(x.getValue() - secondMap.get(x.getKey())))
             .sum();
         
-        return firstNumNeeded + secondNumNeeded + commonNumNeeded;
+        return firstNum + secondNum + commonNum;
     }
 
     private static Map<Character, Integer> createFrequencyMap(String s) {
@@ -44,6 +44,6 @@ public class MakingAnagrams {
         Scanner in = new Scanner(System.in);
         String a = in.next();
         String b = in.next();
-        System.out.println(numberNeeded(a, b));
+        System.out.println(computeLeastNumberOfRemovals(a, b));
     }
 }
