d4841259c69e3bde80de7ee35189f2bea950c959
Shishir Biyyala <shishir.biyyala@gmail.com>
2016-12-06 01:38:18 -0500

    Removed console outputs

--- js/miscellaneous/test/varargs.js
+++ js/miscellaneous/test/varargs.js
@@ -1,13 +1,15 @@
 var expect = require('chai').expect;
 
-/* function sum_varargs(...args) {
- *   
- *   if (args.length === 2) {
- *     return args.reduce((a, b) => a+b, 0);
- *   } else (args.length > 0) {
- *     return x => args[0]+x;
- *   }
- * }*/
+function sum_varargs(...args) {
+  
+  if (args.length === 2) {
+    return args.reduce((a, b) => a+b, 0);
+  } else if(args.length > 0) {
+    return x => args[0]+x;
+  } else {
+    return -1;
+  }
+}
 
 function sum(a, b) {
   
@@ -18,11 +20,10 @@ function sum(a, b) {
       return a+b;
     }
   }
-  
 }
 
-console.log(sum(5, 9));
-console.log(sum(5)(9));
+expect(sum(5, 9)).to.equal(14);
+expect(sum(5, 9)).to.equal(sum(5)(9));
 
-/* console.log(sum_varargs(5, 9));
- * console.log(sum_varargs(5)(9));*/
+expect(sum_varargs(5, 9)).to.equal(14);
+expect(sum_varargs(5, 9)).to.equal(sum_varargs(5)(9));
